59bf0394d4898715cbc51b1ceca9456152d6d232
NCSavage01 <cortavias.hatcher@yahoo.com>
2023-07-30 12:02:59 -0400

    Add basic files

---
+++ index.js
@@ -0,0 +1,21 @@
+var MyQuestions =[
+{Question : "What anime is this opening from?", 
+Answers: {
+    A: 'Naruto' ,
+    B: 'Fairy Tail' ,
+    C: 'Dragon Ball Z' ,
+    D: 'Re-zero'
+},
+CorrectAnswer: 'D'
+},
+
+{Question: "Finish the phrase: "It's time to go beyond _____"?", 
+Answers: {
+    A: 'My Hero Academia' ,
+    B: 'Fairy Tail' ,
+    C: 'Dragon Ball Z' ,
+    D: 'Re-zero'
+},
+CorrectAnswer: 'D'
+},
+];

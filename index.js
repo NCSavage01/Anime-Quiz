37a7f5e921216b43f6825e2beb4f8e6d4ff157d9
NCSavage01 <cortavias.hatcher@yahoo.com>
2023-08-03 21:42:11 -0400

    Edited home screen

--- index.js
+++ index.js
@@ -32,17 +32,116 @@ const myQuestions =[
     CorrectAnswer: 'A' 
     },
     ];
+
+
+
+
     // function GetAnswers(myQuestions) {
     //     return Answers;
     //   }
     //   function BeginQuiz(myQuestions, answerForQuestions)
     //   const answerForQuestions = document.getElementById("Answers");
 
- let questionsP = document.getElementById("questions")     
 
- questionsP.textContent = `${myQuestions[0].Question}`
+    // function start(){
 
-console.log(questionsP)
+    // }
+
+    //question and answers choices appear when you click start
+
+
+    //loop over the questions array,
+// for each question we'll have a condition
+
+
+// call a function that lets you answer the quesiton
+// if the answer that user clicks on is correct, then ....
+/// if not correct, then ....
+
+
+
+
+ let start = document.getElementById("Start")
+
+let answerQuestion = start.addEventListener("click", function() {
+
+  console.log("start works")
+
+  //want questions and answers to show up in Questions div
+  let questionsP = document.getElementById("Questions")     
+
+  questionsP.innerHTML = `<p>${myQuestions[0].Question}</p>
+  
+  <div id="1">${myQuestions[0].Question}</div>
+  <div id="1a">${myQuestions[0].Answers.A}</div>
+  <div id="1b">${myQuestions[0].Answers.B}</div>
+  <div id="1c">${myQuestions[0].Answers.C}</div>
+  <div id="1d">${myQuestions[0].Answers.D}</div>
+  `
+
+  let answer1A = document.getElementById("1a")
+
+  let answer1D = document.getElementById("1d")
+
+
+answer1A.addEventListener("click", e => {
+
+  if (e.target.id = "1a") {
+    {else
+
+      answer1A.style.color = "red"
+      answerID.style.color = "green"
+    
+
+}
+
+});
+
+
+
+
+
+    //change the answer color to red
+
+
+    //calls this function:    maybe later
+    //changeColor()
+   
+   
+    //if answer choice is clicked 
+    // then the color o fthe answer choice becomes red
+
+ 
+
+
+    // shows solution message ... strecth 
+
+
+
+
+
+})
+
+// let answer = document.getElementById("See Solution")
+// let Answers = start.addEventListener("click", '1a'{
+//     console.log("A")
+//     let CorrectAnswer = document.getElementById("Answer")
+// });
+
+
+
+// function changeColor () {
+
+  //once user hits wrong answer changecolor to red, when right answer clicked changecolor to green
+  // display next question
+
+// }
+
+
+
+
+
+// console.log(questionsP)
 
 //  questionsP.textContent = "hello"
 
